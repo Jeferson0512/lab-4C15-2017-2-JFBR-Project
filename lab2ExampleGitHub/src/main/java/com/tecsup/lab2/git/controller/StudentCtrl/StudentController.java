df2c8cfa664732ee6b485387a1a712ce6f18aac4
Alumno <Alumno@LAB402-06>
2017-08-22 10:37:57 -0500

    Inicio de proyecto

---
+++ lab2ExampleGitHub/src/main/java/com/tecsup/lab2/git/controller/StudentCtrl/StudentController.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab2.git.controller.StudentCtrl;
+
+/**
+ * Hello world!
+ *
+ */
+public class StudentController 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
